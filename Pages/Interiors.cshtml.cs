d778be0edbea316104748542018f6cca8d7e2ed2
davidramsayMS <davidramsay@microsoft.com>
2024-04-09 10:17:06 -0400

    Add project files.

---
+++ Pages/Interiors.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Landscaping.Pages
+{
+    public class InteriorsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
